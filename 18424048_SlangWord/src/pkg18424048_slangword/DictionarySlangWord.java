d81d2ba6fe4f44d5278832a8cf565bab92e250f8
NhonPham <nhonpham0211@gmail.com>
2022-07-05 01:37:38 +0700

    create import data function from file

--- 18424048_SlangWord/src/pkg18424048_slangword/DictionarySlangWord.java
+++ 18424048_SlangWord/src/pkg18424048_slangword/DictionarySlangWord.java
@@ -4,8 +4,14 @@
  */
 package pkg18424048_slangword;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -29,6 +35,39 @@ public class DictionarySlangWord {
 
     public void setSlangWordList(List<Word> SlangWordList) {
         this.SlangWordList = SlangWordList;
+    }
+
+    public void ImportSlangWordListFromTXT(Map<String, List<String>> DB) {
+
+        String txtFileName = "slang.txt";
+
+        try ( BufferedReader br = new BufferedReader(new FileReader(txtFileName))) {
+            String contentLine = "";
+            String SplitBy = "`";
+            while ((contentLine = br.readLine()) != null) {
+
+                if (contentLine.contains(SplitBy) == true) {
+                    String[] arr = contentLine.split(SplitBy);
+                    Word w = new Word();
+                    w.setSlangWord(arr[0]);
+                    w.setMean(arr[1]);
+                    Collections.addAll(SlangWordList, w);
+
+                    for (int i = 0; i < SlangWordList.size(); i++) {
+                        List<String> ListMeanValid;
+                        ListMeanValid = new ArrayList<>();
+                        String[] validMean = SlangWordList.get(i).getMean().split("\\|");
+                        ListMeanValid.addAll(Arrays.asList(validMean));
+                        DB.put(w.getSlangWord(), ListMeanValid);
+                    }
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+
+            System.out.println("File not found or File error!");
+        }
+
     }
 
 }
