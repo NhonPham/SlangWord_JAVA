0e6fc0df0410ea8de718aa13add5ccf6d165e03f
NhonPham <nhonpham0211@gmail.com>
2022-07-05 00:30:20 +0700

    Create project

---
+++ 18424048_SlangWord/src/pkg18424048_slangword/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg18424048_slangword;
+
+/**
+ *
+ * @author 18424048 Pham Huu Nhon
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
