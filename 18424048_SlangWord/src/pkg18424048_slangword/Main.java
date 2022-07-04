013d2dcb915ff55f4c7bafe724ea960f42ac83ff
NhonPham <nhonpham0211@gmail.com>
2022-07-05 01:08:00 +0700

    Declare a hashmap to store data and Create Menu function

--- 18424048_SlangWord/src/pkg18424048_slangword/Main.java
+++ 18424048_SlangWord/src/pkg18424048_slangword/Main.java
@@ -4,6 +4,10 @@
  */
 package pkg18424048_slangword;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author 18424048 Pham Huu Nhon
@@ -15,6 +19,24 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Map<String, List<String>> DB = new HashMap<>();
     }
-    
+
+    public static void Menu() {
+        System.out.println("------------------MENU------------------");
+        System.out.println("0. Show list of all Slang Word");
+        System.out.println("1. Find by slang word ");
+        System.out.println("2. Find by the definition");
+        System.out.println("3. Show history, see the list of searched words");
+        System.out.println("4. Add a new Slang Word");
+        System.out.println("5. Eddit a Slang Word");
+        System.out.println("6. Delete a Slang Word");
+        System.out.println("7. Reset the Slang Word list");
+        System.out.println("8. On this day slang word");
+        System.out.println("9. Quiz With Random Slang Word");
+        System.out.println("10. Quiz With Random Definition");
+        System.out.println("11. Exit");
+
+        System.out.print("Enter the number to select: ");
+    }
 }
